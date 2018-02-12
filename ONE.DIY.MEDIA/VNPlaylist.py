df3f7317e3435f99b9850c63399cdd7516afb7b6
tcm21DIY <35118810+tcm21DIY@users.noreply.github.com>
2018-02-11 21:41:01 -0500

    Update VNPlaylist.py

--- ONE.DIY.MEDIA/VNPlaylist.py
+++ ONE.DIY.MEDIA/VNPlaylist.py
@@ -22,7 +22,7 @@ sheet_headers  = {
 }
 
 def GetSheetIDFromSettings():
-	sid = "1LuGrvHlx_kuXVAmYmBvKHb7RJBpF0BkwXg8z7lILhTk"
+	sid = "12_WhMHe_FmmQZ6V4Ll6GFMJNR5wbrNJfmC5JNQRjL2A"
 	resp, content = http.request(plugin.get_setting("GSheetURL"),"HEAD")
 	try:
 		sid = re.compile("/d/(.+?)/").findall(resp["content-location"])[0]
@@ -168,7 +168,7 @@ def getItems(url_path="0", tq="select A,B,C,D,E"):
 			item["path"] = pluginrootpath + "/executebuiltin/-"
 		else:
 			if "spreadsheets/d/" in item["path"]:
-				# https://docs.google.com/spreadsheets/d/1LuGrvHlx_kuXVAmYmBvKHb7RJBpF0BkwXg8z7lILhTk/edit#gid=0
+				# https://docs.google.com/spreadsheets/d/12_WhMHe_FmmQZ6V4Ll6GFMJNR5wbrNJfmC5JNQRjL2A/edit#gid=0
 				match_cache = re.search('cache=(.+?)($|&)',item["path"])
 				match_passw = re.search('passw=(.+?)($|&)',item["path"])
 
